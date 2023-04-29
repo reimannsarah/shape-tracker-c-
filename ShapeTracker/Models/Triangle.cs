b2e5d9a2b737dcb5c0c7af34bb9a0d378edf1ebf
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-29 09:26:22 -0700

    add Side2 functionality + passing test

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,17 +1,19 @@
 namespace ShapeTracker.Models
 {
-  public class Triangle
-  {
-    private int _side1;
-    public int Side1
+    public class Triangle
     {
-      get { return _side1; }
-      set { _side1 = value; }
-    }
+        private int _side1;
+        public int Side1
+        {
+            get { return _side1; }
+            set { _side1 = value; }
+        }
+        public int Side2 { get; }
 
-    public Triangle(int length1)
-    {
-      _side1 = length1;
+        public Triangle(int length1, int length2)
+        {
+            _side1 = length1;
+            Side2 = length2;
+        }
     }
-  }
 }
