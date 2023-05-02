1ab50c6fb549ef3ea12269077d211eab257145b2
Sarah Reimann <reimannsarah45@gmail.com>
2023-05-02 06:22:47 -0700

    add overloaded Triangle constructor to include guess option

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,32 +1,72 @@
+using System.Collections.Generic;
+
 namespace ShapeTracker.Models
 {
-    public class Triangle
+  public class Triangle
+  {
+    private int _side1;
+    public int Side1
     {
-        private int _side1;
-        public int Side1
-        {
-            get { return _side1; }
-            set { _side1 = value; }
-        }
-        public int Side2 { get; set; }
-        private int _side3;
-        public Triangle(int length1, int length2, int length3)
-        {
-            _side1 = length1;
-            Side2 = length2;
-            _side3 = length3;
-        }
-        public int GetSide3()
-        {
-            return _side3;
-        }
-        public void SetSide3(int newValue)
-        {
-            _side3 = newValue;
-        }
-        public string CheckType()
-        {
-            return "not a triangle";
-        }
+      get { return _side1; }
+      set { _side1 = value; }
     }
+    public int Side2 { get; set; }
+    private int _side3;
+    private static List<Triangle> _instances = new List<Triangle> {};
+    public string Guess { get; set; }
+
+
+    public Triangle(int length1, int length2, int length3)
+    {
+      _side1 = length1;
+      Side2 = length2;
+      _side3 = length3;
+      _instances.Add(this);
+    }
+    public Triangle(int length1, int length2, int length3, string userGuess) : this(length1, length2, length3)
+    {
+      Guess = userGuess;
+    }
+
+    public int GetSide3()
+    {
+      return _side3;
+    }
+
+    public void SetSide3(int newValue)
+    {
+      _side3 = newValue;
+    }
+
+    public string CheckType()
+    {
+      if ((Side1 > (Side2 + _side3)) || (Side2 > (Side1 + _side3)) || (_side3 > (Side1 + Side2)))
+      {
+        return "not a triangle";
+      } 
+      else if ((Side1 != Side2) && ((Side1 != _side3)) && ((Side2 != _side3))) 
+      {
+        return "scalene triangle";
+      }  
+      else if ((Side1 == Side2) && (Side1 == _side3)) 
+      {
+        return "equilateral triangle";
+      } 
+      else 
+      {
+        return "isosceles triangle";
+      }
+    }
+
+    public static List<Triangle> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+  }
 }
